045c98eec8c89f7e0fcdad75a4f650319e2f7e33
LVT0529 <dlqlrxh2@naver.com>
2018-04-18 13:01:24 +0900

    SugarDelivery
    아직 미완성

--- BaekJoon_Practice/src/Inputoutput/p2557_HelloWorld.java
+++ BaekJoon_Practice/src/Inputoutput/p2557_HelloWorld.java
@@ -3,5 +3,11 @@ package Inputoutput;
 public class p2557_HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		int a = 10, b = 5, c;
+		
+		c = a%b;
+		
+		System.out.println(c);
 	}
 }
--- BaekJoon_Practice/src/Manipulation/p2839_SugerDelivery.java
+++ BaekJoon_Practice/src/Manipulation/p2839_SugerDelivery.java
@@ -1,5 +1,35 @@
 package Manipulation;
 
+import java.util.Scanner;
 public class p2839_SugerDelivery {
-
+	public static void main(String[] args) {
+		int count = 0;
+		int total_k;
+		
+		Scanner input = new Scanner(System.in);
+		
+		total_k = input.nextInt();
+		
+		
+		if(total_k % 5 == 0) { 
+			System.out.println(total_k/5);
+		}
+		else if(total_k % 5 != 0 || total_k % 3 != 0)
+		{
+			count = total_k/5;
+			total_k %= 5;
+			if(total_k % 3 == 0)
+				count += total_k/3;
+			else
+				count = -1;
+			
+			System.out.println(count);
+		}
+		else
+		{
+			System.out.println(total_k/3);
+		}
+			
+		input.close();
+	}
 }
