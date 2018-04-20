db1a19a280ed9ba4946916da35f791bbb68c088e
LVT0529 <dlqlrxh2@naver.com>
2018-04-20 12:37:43 +0900

    미완성 ㅠㅠ
    ㅇ

--- BaekJoon_Practice/src/Manipulation/p2839_SugerDelivery.java
+++ BaekJoon_Practice/src/Manipulation/p2839_SugerDelivery.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class p2839_SugerDelivery {
 	public static void main(String[] args) {
 		int count = 0;
-		int total_k;
+		int total_k, temp;
 		
 		Scanner input = new Scanner(System.in);
 		
@@ -14,20 +14,31 @@ public class p2839_SugerDelivery {
 		if(total_k % 5 == 0) { 
 			System.out.println(total_k/5);
 		}
-		else if(total_k % 5 != 0 || total_k % 3 != 0)
-		{
-			count = total_k/5;
-			total_k %= 5;
-			if(total_k % 3 == 0)
-				count += total_k/3;
-			else
-				count = -1;
-			
-			System.out.println(count);
-		}
 		else
 		{
-			System.out.println(total_k/3);
+			temp = total_k;
+			while(total_k > 0) {
+				total_k -= 5;
+				count++;
+				
+				if((total_k-5)%3 ==0) {
+					count++;
+					total_k -= 5;
+					count += total_k/3;
+					total_k %= 3;
+				}
+				
+				if(total_k % 3 == 0 && total_k >= 3) {				
+					count += total_k/3;
+					total_k %= 3;
+				}
+			}
+			if(total_k == 0)
+				System.out.println(count);
+			else if(temp % 3 == 0)
+				System.out.println(temp/3);
+			else
+				System.out.println("-1");
 		}
 			
 		input.close();
